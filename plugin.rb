df48562e97f52575db5dc907cc3cf43c1b5d90a8
benkeks <benjamin.bisping@tu-berlin.de>
2020-12-30 18:20:56 +0100

    Bake each proxy's voting key into post and enforce maximum

--- plugin.rb
+++ plugin.rb
@@ -17,6 +17,7 @@ PLUGIN_NAME ||= 'DiscourseDemocracy'
 load File.expand_path('lib/discourse-democracy/engine.rb', __dir__)
 load File.expand_path('config/routes.rb', __dir__)
 
+load File.expand_path('../poll/app/models/poll_vote.rb', __dir__)
 
 # register_editable_user_custom_field :my_preference 
 # DiscoursePluginRegistry.serialized_current_user_fields << 'my_preference'
@@ -32,6 +33,7 @@ after_initialize do
   register_user_custom_field_type :dem_delegations, :string
   #register_editable_user_custom_field :dem_proxy_mandates
   register_user_custom_field_type :dem_proxy_mandates, :string
+  register_post_custom_field_type :dem_proxy_mandates, :string
   
   add_to_class(:user, :dem_delegations) do
     custom_fields['dem_delegations'] && JSON.parse(custom_fields['dem_delegations']) || {}
@@ -41,40 +43,61 @@ after_initialize do
     custom_fields['dem_proxy_mandates'] && JSON.parse(custom_fields['dem_proxy_mandates']) || {}
   end
 
+  add_to_class(:post, :dem_proxy_mandates) do
+    custom_fields['dem_proxy_mandates'] && JSON.parse(custom_fields['dem_proxy_mandates']) || {}
+  end
+
+  add_model_callback(:poll_vote, :after_create, {}) do
+    # “Freeze” the delegations of a proxy for each poll, so that changes in delegations cannot change a voting result ex-post.
+    voter = User.find_by(id: self.user_id)
+
+    if mandates = voter&.dem_proxy_mandates['*']
+      post_id = PollVote.where(poll_id: self.poll_id).joins(:poll).select(:post_id).take.post_id
+      post = Post.find_by(id: post_id)
+      new_mandates = post.dem_proxy_mandates
+      new_mandates["#{self.poll_id},#{self.user_id}"] = mandates
+      post.custom_fields['dem_proxy_mandates'] = new_mandates
+      post.save_custom_fields(true)
+      post.reload
+    end
+  end
+
   add_to_serializer(:user, :dem_delegations) { object.dem_delegations }
 
   add_to_serializer(:user, :dem_proxy_mandates) { object.dem_proxy_mandates }
 
   add_to_serializer(:post, :polls_delegated_votes) do
-    if preloaded_polls
+    if preloaded_polls && (mandates = object.dem_proxy_mandates)
       preloaded_polls.map do |poll|
-        user_poll_votes =
-          poll
-            .poll_votes
-            .joins(:poll_option, :user)
-            .select('poll_options.digest,users.username,user_id,poll_option_id')
-
-        poll_delegated_votes = user_poll_votes.flat_map do |vote|
-          if proxy = User.find_by(id: vote['user_id'])
-            # select all ids of users who delegated their vote to a voter but did not (yet) cast votes in this poll themselves.
-            # TODO: It would be desirabe to “freeze” the delegations of a proxy for each poll, so that changes in delegations cannot change a voting result ex-post.
-            additional_vote_ids = proxy.dem_proxy_mandates['*']&.select do |mandator_id|
-              !user_poll_votes.any? { |vote| vote.user_id == mandator_id }
-            end
-
-            #additional_vote_ids.map { |id| User.find_by(id: id)) }
-            delegated_votes_cast = User.where(id: additional_vote_ids).map { |u| UserNameSerializer.new(u).serializable_hash }
-            if delegated_votes_cast && !delegated_votes_cast.empty?
-              [{ delegated_votes: delegated_votes_cast, ids: additional_vote_ids, parent_vote: vote}]
+        if poll.visibility == "everyone"
+          user_poll_votes =
+            poll
+              .poll_votes
+              .joins(:poll_option, :user)
+              .select('poll_options.digest,users.username,user_id,poll_option_id')
+          poll_delegated_votes = user_poll_votes.flat_map do |vote|
+            if proxys_mandates = mandates["#{poll.id},#{vote.user_id}"]
+              # select all ids of users who delegated their vote to a voter but did not (yet) cast votes in this poll themselves.
+              additional_vote_ids = proxys_mandates.select do |mandator_id|
+                !user_poll_votes.any? { |vote| vote.user_id == mandator_id }
+              end
+              # trim to maximal effective delegations per proxy vote
+              additional_vote_ids = additional_vote_ids.take(SiteSetting.discourse_democracy_max_effective_delegations)
+              # also return info on the voters whose votes were decided by proxy
+              delegated_votes_cast = User.where(id: additional_vote_ids).map { |u| UserNameSerializer.new(u).serializable_hash }
+              if delegated_votes_cast && !delegated_votes_cast.empty?
+                [{ delegated_votes: delegated_votes_cast, ids: additional_vote_ids, parent_vote: vote}]
+              else
+                []
+              end
             else
               []
             end
-          else
-            []
           end
+          [poll.name, poll_delegated_votes]
+        else
+          [poll.name, []]
         end
-        
-        [poll.name, poll_delegated_votes]
       end.to_h
     else
       {}
